9b4ef31eb7308499bf7a0349689e742caa389436
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-20 18:44:08 -0400

    Connected html files to routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,24 @@
 // Server side
+// Call in packages
 const express = require('express');
 const path = require('path');
 const app = express();
 
+// Initialize middleware
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
 
+// Return nodes.html when /notes called
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// Return index.html whenever anything else is called
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+// Return db.json when at /api/notes
 app.get('/api/notes', (req, res) =>{
   res.sendFile(path.join(__dirname, '/db/db.json'));
 });
