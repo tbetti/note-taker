54448a3fac134efa01c341e25f122cb0fff3867a
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-20 19:02:55 -0400

    Created post HTTP request for new note

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,35 @@
 // Call in packages
 const express = require('express');
 const path = require('path');
+const uuid = require('uuid');
 const app = express();
+const notes = require('./db/db.json');
 
 // Initialize middleware
 app.use(express.json());
 app.use(express.urlencoded( { extended: false }));
+
+// Return db.json when at /api/notes
+// Return all notes
+app.get('/api/notes', (req, res) =>{
+  console.log(notes);
+  res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+// Create a new note
+app.post('/api/notes', (req, res) => {
+  const newNote = {
+    id: uuid.v4().substr(0,3),
+    title: req.body.title,
+    text: req.body.text
+  }
+  if(newNote.title && newNote.text){
+    notes.push(newNote);
+    res.json(notes);
+  }else{
+    res.status(400).json( { msg: 'Include title and text' } );
+  }
+})
 
 // Return nodes.html when /notes called
 app.get('/notes', (req, res) => {
@@ -14,14 +38,10 @@ app.get('/notes', (req, res) => {
 });
 
 // Return index.html whenever anything else is called
+// Must stay at the bottom
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 })
-
-// Return db.json when at /api/notes
-app.get('/api/notes', (req, res) =>{
-  res.sendFile(path.join(__dirname, '/db/db.json'));
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
