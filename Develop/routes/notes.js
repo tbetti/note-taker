aeb05b68653e33a83c7bd0322123828e10c1b594
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-23 10:28:08 -0400

    Handled data persistance when deleting entry

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 const { isBuffer } = require('util');
 const uuid = require('uuid');
-const notes = require('../db/db.json');
+let notes = require('../db/db.json');
 
 // Return all notes
 router.get('/', (req, res) => {
@@ -30,21 +30,14 @@ router.post('/', (req, res) => {
         text: req.body.text
     }
     if (newNote.title && newNote.text) {
-        fs.readFile('./db/db.json', 'utf8', (err, data) =>{
-            if(err){
-                console.error(err);
-            }else{
-                // Parse data and push new entry
-                const parsedNote = JSON.parse(data);
-                parsedNote.push(newNote);
-                //Overwrite existing file with new data
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parsedNote, null, ' '),
-                    error => error ? console.error(error) : console.info('Data updated!')
-                );
-            }
-        });
+        // push new entry
+        notes.push(newNote)
+        //Overwrite existing file with new data
+        fs.writeFile(
+            './db/db.json',
+            JSON.stringify(notes, null, ' '),
+            error => error ? console.error(error) : console.info('Data updated!')
+        );
         res.json(notes);
     } else {
         res.status(400).json({ msg: 'Include title and text' });
@@ -56,7 +49,13 @@ router.delete('/:id', (req, res) => {
     const found = notes.some(note => note.id === req.params.id);
 
     if (found) {
-        res.json(notes.filter(note => note.id !== req.params.id));
+        notes = notes.filter(note => note.id !== req.params.id);
+        fs.writeFile(
+            './db/db.json',
+            JSON.stringify(notes, null, ' '),
+            error => error ? console.error(error) : console.info('Data updated!')
+        );
+        res.json(notes);
     } else {
         res.status(400).json({ msg: `${req.params.id} not found` });
     }
